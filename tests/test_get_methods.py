6ff2d71501e7fd1bf506793e9d616934decfad8b
Nikolai Mitroshkin <ex3ct@mail.ru>
2025-12-13 13:52:15 +0300

    added api methods and new tests

--- tests/test_get_methods.py
+++ tests/test_get_methods.py
@@ -1,24 +1,108 @@
 import os.path
-import utils.utils as u
 
+import utils.utils as u
 from conftest import *
+from utils.checking import Checking
+
 
 class TestGetMethods:
     RESOURCE_NAME = ["albums", "comments", "photos", "posts", "todos", "users"]
 
     @pytest.mark.parametrize("endpoint, expected_data",
                              [(x, u.open_json_file(os.path.join(os.path.dirname(__file__),
-                                                                        f"resources/{x}/all_resources.json")))
+                                                                f"resources/{x}/all_resources.json")))
                               for x in RESOURCE_NAME])
-
     def test_get_all_resources(self, api_client, endpoint, expected_data):
-        print(endpoint) # название ресурса
-        print(expected_data) # ожидаемые данные из JSON
+        print(endpoint)  # название ресурса
+        print(expected_data)  # ожидаемые данные из JSON
 
-        response = api_client.send_get_request(endpoint)
-        assert response.status_code == 200
+        url = f"https://jsonplaceholder.typicode.com/{endpoint}"
+        response = api_client.send_get_request(url)
+        Checking.check_status_code(response, 200)
         assert response.json() is not None
 
-#TODO: 1) продолжение - https://www.youtube.com/watch?v=AFuTzPWEYF4
-#TODO: 2) понять что написали в лябмда функции (test_get_all_resources) / 2) Может удалить этот тест и написать простые другие?
-#TODO: 3) улучшить проект фишками из учебного проекта 4) улучшить проект фишками из рабочего проекта 5) добавить проверки
+    # TODO: 1) улучшить проект фишками из рабочего проекта 2) добавить проверки (нужен Pydantic) 3) спрятать не нужные проекты в гит (можно?)
+
+    def test_get_all_posts(self, api_client):
+        url = "https://jsonplaceholder.typicode.com/posts"
+        response = api_client.send_get_request(url)
+        print(response.json())
+        Checking.check_status_code(response, 200)
+
+
+    def test_get_first_post(self, api_client):
+        url = "https://jsonplaceholder.typicode.com/posts/1"
+        response = api_client.send_get_request(url)
+        print(response.json())
+        Checking.check_status_code(response, 200)
+        Checking.check_json_fields(response, ["userId", "id", "title", "body"])
+        Checking.check_json_values(response, "title", "sunt aut facere repellat provident "
+                                                      "occaecati excepturi optio reprehenderit")
+        Checking.check_json_search_word_in_values(response, "body", "architecto")
+
+
+    def test_get_first_post_comments(self, api_client):
+        url = "https://jsonplaceholder.typicode.com/posts/1/comments"
+        response = api_client.send_get_request(url)
+        print(response.json())
+        Checking.check_status_code(response, 200)
+        Checking.check_json_fields(response, ["postId", "id", "name", "email", "body"])
+        Checking.check_json_values(response, "name",
+                                   "alias odio sit")
+        Checking.check_json_search_word_in_values(response, "body", "harum")
+
+    # TODO: 1) нужны методы проверки значений по response
+
+    def test_send_new_post(self, api_client):
+        url = "https://jsonplaceholder.typicode.com/posts"
+        new_post = {
+            "userId": 13,
+            "id": 1337,
+            "title": "new magic post",
+            "body": "test magic body"
+        }
+        response = api_client.send_post_request(url, new_post)
+        print(response.json())
+        Checking.check_status_code(response, 201)
+        Checking.check_json_fields(response, ["userId", "id", "title", "body"])
+        Checking.check_json_values(response, "title",
+                                   "new magic post")
+        Checking.check_json_search_word_in_values(response, "body", "body")
+
+
+    def test_change_first_post(self, api_client):
+        url = "https://jsonplaceholder.typicode.com/posts/1"
+        new_body = {
+            "userId": 2,
+            "id": 1,
+            "title": "new title",
+            "body": "new body"
+        }
+        response = api_client.send_put_request(url, new_body)
+        print(response.json())
+        Checking.check_status_code(response, 200)
+        Checking.check_json_fields(response, ["userId", "id", "title", "body"])
+        Checking.check_json_values(response, "title",
+                                   "new title")
+        Checking.check_json_search_word_in_values(response, "body", "new body")
+
+
+    def test_change_only_title_for_first_post(self, api_client):
+        url = "https://jsonplaceholder.typicode.com/posts/1"
+        new_title = {
+            "title": "changed only title"
+        }
+        response = api_client.send_patch_request(url, new_title)
+        print(response.json())
+        Checking.check_status_code(response, 200)
+        Checking.check_json_fields(response, ["userId", "id", "title", "body"])
+        Checking.check_json_values(response, "title",
+                                   "changed only title")
+
+
+    def test_delete_first_post(self, api_client):
+        url = "https://jsonplaceholder.typicode.com/posts/1"
+        response = api_client.send_delete_request(url)
+        print(response.json())
+        Checking.check_status_code(response, 200)
+        assert response.json() == {}
--- utils/api_helper.py
+++ utils/api_helper.py
@@ -5,9 +5,32 @@ class APIClient:
     def __init__(self, base_url="https://jsonplaceholder.typicode.com"):
         self.base_url = base_url
 
-    def send_get_request(self, endpoint):
-        url = f"{self.base_url}/{endpoint}"
+
+    def send_get_request(self, url):
         print(f"Итоговый url = {url}")
         response = requests.get(url)
         return response
-    
+
+
+    def send_post_request(self, url, data):
+        print(f"Итоговый url = {url}")
+        response = requests.post(url, json=data)
+        return response
+
+
+    def send_put_request(self, url, data):
+        print(f"Итоговый url = {url}")
+        response = requests.put(url, json=data)
+        return response
+
+
+    def send_patch_request(self, url, data):
+        print(f"Итоговый url = {url}")
+        response = requests.patch(url, json=data)
+        return response
+
+
+    def send_delete_request(self, url):
+        print(f"Итоговый url = {url}")
+        response = requests.delete(url)
+        return response
